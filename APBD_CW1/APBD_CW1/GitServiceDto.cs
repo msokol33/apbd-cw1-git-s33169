267f81dbdbb036ea21df3b2ebdaf653dbb0373a6
msokol33 <s33169@pjwstk.edu.pl>
2026-03-11 11:01:29 +0100

    APBD-8
    + Słownik danych dla GitService

---
+++ APBD_CW1/APBD_CW1/GitServiceDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace APBD_CW1;
+
+[DataContract]
+public class GitServiceDto
+{
+    [DataMember]
+    public string RepoName { get; set; }
+    
+    [DataMember]
+    public string BranchName { get; set; }
+    
+}
